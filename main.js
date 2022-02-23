a49433e4ea94cafc39c27fd0e18a6bb46d8d774c
Sam Nassif <98352078+Snassif1990@users.noreply.github.com>
2022-02-23 17:57:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,65 +1,46 @@
 const prompt = require('prompt-sync')();
 
 const itemArray = [];
-
+let itemArrayComplete = [];
 let itemCount = itemArray.length-1;
+let decision = Number('');
 
-decision = Number(prompt("\n ~ Select an action ~ \n\n[1] Create a to-do item \n[2] Complete a to-do item \n[3] Show to-do list \n[4] End the program \n\n> "));
+while (decision !== 3) {
 
-while(decision !== 4) {
+    console.log("\n ~ Select an action ~ \n\n[1] Create a to-do item \n[2] Complete a to-do item \n[3] Exit the program");
 
+    let decision = Number(prompt("\n> "));
 
-    if (!itemArray.length) {
-        console.log("\nYour to-do list is empty.\n")
-    }
-    
-    // else if (itemArray > 0){
-    //     for (let i = 0; i < itemArray.length; i++){
-    //     console.log(itemArray[i])
-    //     }
-    // }
-
-    if (decision === 1){
-        console.log(" ~ Creating a new to-do item ~ \nWhat is this to-do item called? \n");
+    if (decision === 1) {
+        console.log("\n ~ Creating a new to-do item ~ \nWhat is this to-do item called? \n");
         let newItem = prompt("> ");
-        itemArray.push("[incomplete] " + newItem);
-                
-        for (let i = 0; i < itemArray.length; i++){
-            console.log("\n" + itemArray[i])
+        itemArray.push(newItem);
+        itemArrayComplete.push("[incomplete]")
+
+        console.log("\n===========================================")
+        for (let i = 0; i < itemArray.length; i++) {
+            console.log(`${i+1}. ${itemArrayComplete[i]} ${itemArray[i]}`);
+            // console.log( i + ' + ' + 1 + itemArrayComplete[i] + itemArray[i]);
         }
-
-        decision = Number(prompt("\n ~ Select an action ~ \n\n[1] Create a to-do item \n[2] Complete a to-do item \n[3] Show to-do list \n[4] End the program \n\n> "));
+        console.log("===========================================");
     }
-
     else if (decision === 2){
-        let itemArrayComplete = prompt("Select to-do item to complete: \n\n> ");
-            if(itemArray.includes(itenArrayComplete)){
-                
-                for (const char of str) {
-                    complete += char === 'incomplete'? 'complete' : char;
-                }
-            console.log("\n You have " + itemCount + " to-do item(s).\n\n" + itemArray[i]);
-            }
-            // decision = Number(prompt("\n ~ Select an action ~ \n\n[1] Create a to-do item \n[2] Complete a to-do item \n[3] Show to-do list \n[4] End the program \n\n> "))
-    
-    }
+        console.log("\n ~ Completing a to-do item ~ \nWhich to-do item would you like to complete?");
+        let completeItem = prompt("> ")
+        itemArrayComplete[completeItem -1] = "[complete]";
 
-    else if (decision === 3){
-        if (!itemArray.length) {
-            console.log("\nYour to-do list is empty.\n")
+        console.log("\n===========================================");
+        for (let i = 0; i < itemArray.length; i++) {
+            console.log(`${i+1}. ${itemArrayComplete[i]} ${itemArray[i]}`);
         }
-        if (itemArray > 0){
-            for (let i = 0; i < itemArray.length; i++){
-            console.log(itemArray[i])
-            }
-        }
-    }
+        console.log("===========================================");
 
-    else if (decision === 4){
-        console.log("You have closed the program.")
     }
-
+    else if (decision === 3) {
+        console.log("You have closed the program");
+    }
     else {
-        console.log("ERROR")
+        console.log("\n --- ERROR PLEASE TRY AGAIN --- \n")
+        break;
     }
 }
